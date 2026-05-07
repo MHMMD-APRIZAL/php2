54570f6e426659973582a14c06a347946f5b64a1
MHMMD-APRIZAL <muhammadafrizall050517@gmail.com>
2026-05-07 11:58:50 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 // Mengambil data dari environment variables Railway
 $host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
 $user = getenv('MYSQLUSER') ?: 'root';
-$pass = getenv('MYSQLPASSWORD') ?: 'LZstNChgUtUIzpLIpzjVtJVusCMZscPX';
+$pass = getenv('MYSQLPASSWORD') ?: 'yyWVsDyQJbOgpLGUYxxqdJgbBeDDyicX';
 $db   = getenv('MYSQLDATABASE') ?: 'railway';
 $port = getenv('MYSQLPORT') ?: 3306;
 
