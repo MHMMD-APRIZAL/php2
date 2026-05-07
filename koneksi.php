9bb4df6f497671d65a0edea696c0b913e5cc490e
MHMMD-APRIZAL <muhammadafrizall050517@gmail.com>
2026-05-07 12:14:45 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,15 @@
 <?php
-// PHP akan mencoba mengambil data asli dari Railway dulu, 
-// kalau tidak ada baru pakai data di dalam kutip.
-$host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
-$user = getenv('MYSQLUSER') ?: "root";
-$pass = getenv('MYSQLPASSWORD') ?: "yyWVsDyQJbOgpLGUYxxqdJgbBeDDyicX";
-$db   = getenv('MYSQLDATABASE') ?: "railway";
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$user = getenv('MYSQLUSER') ?: 'root';
+$pass = getenv('MYSQLPASSWORD') ?: '';
+$db   = getenv('MYSQLDATABASE') ?: 'railway';
 $port = getenv('MYSQLPORT') ?: 3306;
 
 $koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$koneksi) {
-    // Menampilkan error yang lebih spesifik jika gagal
-    die("Koneksi gagal: " . mysqli_connect_error());
+    header('Content-Type: application/json');
+    echo json_encode(["status" => "error", "message" => "Koneksi Gagal: " . mysqli_connect_error()]);
+    exit;
 }
 ?>
